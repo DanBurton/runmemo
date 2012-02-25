809d9966a91513d61cb8da8cdd28205d01cb796c
Dan Burton <danburton.email@gmail.com>
2012-02-25 00:13:40 -0700

    Test now prints time measured

--- test/test-race.hs
+++ test/test-race.hs
@@ -4,6 +4,7 @@ import Data.MemoCombinators as Memo
 import Data.RunMemo
 import System.Exit
 import Data.Time.Clock
+import Text.Printf
 
 fib :: Memoizable (Integer -> Integer)
 fib self = go
@@ -22,8 +23,10 @@ main :: IO ()
 main = do
   putStrLn "Evaluating with Memo.integral"
   mtime <- time $ runMemo Memo.integral fib 30 `seq` putStrLn "Evaluated"
+  printf "Memo.integral took %s\n" (show mtime)
   putStrLn "Evaluating with noMemo"
   ntime <- time $ runMemo noMemo fib 30 `seq` putStrLn "Evaluated"
+  printf "noMemo        took %s\n" (show ntime)
   if mtime < ntime
     then exitSuccess
     else exitFailure
