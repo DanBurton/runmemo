e117117d9c7b4cd0c1bb70bef19adf1b0eddd8a7
Dan Burton <danburton.email@gmail.com>
2012-02-24 17:55:18 -0700

    Create real test.

--- test/test-race.hs
+++ test/test-race.hs
@@ -1,7 +1,29 @@
 module Main where
 
-import System.Exit (exitFailure)
+import Data.MemoCombinators as Memo
+import Data.RunMemo
+import System.Exit
+import Data.Time.Clock
 
+fib :: Memoizable (Integer -> Integer)
+fib self = go
+  where go 0 = 1
+        go 1 = 1
+        go n = self (n-1) + self (n-2)
+
+time :: IO () -> IO NominalDiffTime
+time a = do
+  start <- getCurrentTime
+  a
+  stop <- getCurrentTime
+  return $ diffUTCTime stop start
+
+main :: IO ()
 main = do
-  putStrLn "This test always fails!"
-  exitFailure
+  putStrLn "Evaluating with Memo.integral"
+  mtime <- time $ runMemo Memo.integral fib 30 `seq` putStrLn "Evaluated"
+  putStrLn "Evaluating with noMemo"
+  ntime <- time $ runMemo noMemo fib 30 `seq` putStrLn "Evaluated"
+  if mtime < ntime
+    then exitSuccess
+    else exitFailure
