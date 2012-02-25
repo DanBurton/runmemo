932517a63501f083620b5c848ba16a54d6848040
Dan Burton <danburton.email@gmail.com>
2012-02-24 17:07:16 -0700

    Adding initial code, cabalized

---
+++ src/Data/RunMemo.hs
@@ -0,0 +1,14 @@
+module Data.RunMemo (
+    runMemo
+  , Memoizable
+  , Memoizer
+  ) where
+
+type Memoizable a = a -> a
+type Memoizer a b = (a -> b) -> a -> b
+
+runMemo :: Memoizer a b -> Memoizable (a -> b) -> a -> b
+runMemo memo f = fix (f . memo)
+  where fix h = let x = h x in x
+        (g . h) x = g (h x)
+
